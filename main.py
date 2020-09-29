3bed77b542e23a0681bb55c3d584669aaa9c7929
WavenZ <1435595081@qq.com>
2020-09-29 10:38:31 +0800

    update

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import angest.est as ae
+import estimate.est as ae
 import cv2
 import os
 
 from cv2 import cv2
 
 def get_mse(real, predict):
-    """
-    均方误差 估计值与真值 偏差
-    """
+    """均方误差 估计值与真值 偏差"""
+
     return sum([(pred - real) ** 2 for pred in predict]) / len(predict)
 
 
 if __name__ == "__main__":
 
-    file_path = r'./graph/dynamic/5dps/100ms/30/'
+    # file_path = r'./graph/dynamic/5dps/100ms/30/'
+    file_path = r'./graph/'
     images = os.listdir(file_path)
     directions = []
     for image in images:
@@ -29,10 +29,12 @@ if __name__ == "__main__":
     print('[Min] ', np.min(directions))
     print('[Max] ', np.max(directions))
     print('[Avg] ', np.mean(directions))  
-    print('[Mse] ', get_mse(60, directions))
+    print('[Mse] ', get_mse(45, directions))
 
-    x = np.linspace(0, len(images), len(images), endpoint=False)
-    plt.figure()
-    plt.ylim((-1, 1))
-    plt.scatter(x, np.array(directions) - 60)
-    plt.show()
+    plot = False
+    if plot:
+        x = np.linspace(0, len(images), len(images), endpoint=False)
+        plt.figure()
+        plt.ylim((-1, 1))
+        plt.scatter(x, np.array(directions) - 45)
+        plt.show()
